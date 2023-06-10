a6c477a176542456eb94663c2ce766730ad0102d
meghanakodimyala <136172613+meghanakodimyala@users.noreply.github.com>
2023-06-10 14:02:54 +0100

    terraform script for ec2 (#1)

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable region {
+  type = string
+  default = "us-east-1"
+
+}
+
+
+variable accesskey {
+  type = string
+  default = "AKIA42R7XPNAHWYYI33B"
+
+}
+
+variable secretkey {
+  type = string
+  default = "MDffHZj5xNIBA3vjpPFaOoH3kcQmT1OudVcbE/+J"
+
+}
