5af67b45abd4e494230cea3d1f8ccce706e8f802
meghanakodimyala <136172613+meghanakodimyala@users.noreply.github.com>
2023-06-10 13:56:05 +0100

    terraform script for ec2

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region     = "us-west-1"
+  access_key = "AKIA42R7XPNAHWYYI33B"
+  secret_key = "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"
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0563db429435a8b9c"
+  instance_type = "t3.micro"
+availability_zone    = "us-west-1b"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+
+
